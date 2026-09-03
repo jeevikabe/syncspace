d60e5be2b3c6473dbf52ce5e9e04d61364c422fb
jeevikabe <bejeevika@gmail.com>
2026-09-03 21:05:11 +0530

    app jsx file updated

--- rtc-backend/server.js
+++ rtc-backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const http = http = require("http"); // or standard http setup
+const http = require("http"); // or standard http setup
 const { Server } = require("socket.io");
 const cors = require("cors");
 
